8d7d35a6cb5805255098107fd73b24256bbcee5b
Milosav <milosavmatic@yahoo.com>
2023-09-28 16:30:22 +0200

    Fixed images in New Category Component

--- components/NewCategoriesSection/NewCategoriesSection.jsx
+++ components/NewCategoriesSection/NewCategoriesSection.jsx
@@ -17,8 +17,8 @@ const NewCategoriesSections = async () => {
             <h2 className='font-bold text-2xl mb-7 text-[#171717]'>Izdvojeno iz nove kolekcije</h2>
             <div className='grid lg:grid-cols-[1fr,2fr,1fr] gap-3'>
                 {newCategories?.slice(0, 5)?.map((category, index) => (
-                    <Link key={category.id} className={`${index === 1 ? 'row-span-2' : ''} aspect-square relative w-full`} href={`/kategorije/${category?.slug_path}`}>
-                      {category?.images?.image && (  <Image src={category?.images?.image} alt='category' fill />)}
+                    <Link key={category.id} className={`${index === 1 ? 'row-span-2 h-full' : ''} aspect-square relative w-full`} href={`/kategorije/${category?.slug_path}`}>
+                      {category?.images?.image && (  <Image src={category?.images?.image} alt='category' fill className='object-cover' />)}
                         <div className='absolute bottom-0 left-0 w-full h-14 bg-black/40 z-10 flex items-center justify-center'>
                             <h3 className='text-white text-center text-[33px] font-light  uppercase'>{category?.basic_data?.name}</h3>
                         </div>
