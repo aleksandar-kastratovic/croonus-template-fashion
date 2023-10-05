b95b587e6ad200f09c48036172a76936c42dab44
Stefan Milijanovic <stefan.milijanovic@croonus.com>
2023-10-05 08:06:08 +0200

    Global - Update na Next 13.5.4

--- components/Header/SliderHeader.jsx
+++ components/Header/SliderHeader.jsx
@@ -1,22 +1,25 @@
-"use client"
-import React, { useState } from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay } from 'swiper';
-import 'swiper/css';
-import LeftIcon from '../svg/LeftIcon';
-import RightIcon from '../svg/RightIcon';
+"use client";
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper";
+import "swiper/css";
+import LeftIcon from "../svg/LeftIcon";
+import RightIcon from "../svg/RightIcon";
 
 const SliderHeader = () => {
   const [swiper, setSwiper] = useState(null);
-  const banners = ['Besplatna isporuka za iznos preko 5.000 RSD', 'Besplatna isporuka za iznos preko 10.000 RSD']
+  const banners = [
+    "Besplatna isporuka za iznos preko 5.000 RSD",
+    "Besplatna isporuka za iznos preko 10.000 RSD",
+  ];
 
   return (
-    <div className=' max-w-[30%] justify-between flex items-center' >
-      <button className='' type='button' onClick={() => swiper.slidePrev()} >
+    <div className=" max-w-[30%] justify-between flex items-center">
+      <button className="" type="button" onClick={() => swiper.slidePrev()}>
         <LeftIcon />
       </button>
       <Swiper
-        className='overflow-hidden max-w-[80%]'
+        className="overflow-hidden max-w-[80%]"
         spaceBetween={50}
         slidesPerView={1}
         onSwiper={(swiper) => setSwiper(swiper)}
@@ -29,19 +32,19 @@ const SliderHeader = () => {
         getswiper={setSwiper}
         centeredSlides={true}
       >
-        {
-          banners?.map((banner, index) =>
-            <SwiperSlide key={index}>
-              <p className='text-sm font-normal text-black bg-topHeader'>{banner}</p>
-            </SwiperSlide>
-          )
-        }
+        {banners?.map((banner, index) => (
+          <SwiperSlide key={index}>
+            <p className="text-sm font-normal text-black bg-topHeader">
+              {banner}
+            </p>
+          </SwiperSlide>
+        ))}
       </Swiper>
-      <button className='' type='button' onClick={() => swiper.slideNext()} >
+      <button className="" type="button" onClick={() => swiper.slideNext()}>
         <RightIcon />
       </button>
     </div>
-  )
-}
+  );
+};
 
-export default SliderHeader
+export default SliderHeader;
