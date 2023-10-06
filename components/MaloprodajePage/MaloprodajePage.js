d8ee4d2344d25608ea8389ccf7c88cc11b7e4ff7
Stefan Milijanovic <stefan.milijanovic@croonus.com>
2023-10-06 14:53:42 +0200

    Kategorije - obrisan hover efekat na breadcrumbs Maloprodaje - ako ima vise od 4 objekta, prikazuje se select

--- components/MaloprodajePage/MaloprodajePage.js
+++ components/MaloprodajePage/MaloprodajePage.js
@@ -49,49 +49,78 @@ const MaloprodajePage = () => {
             potrebno za vaš dom. Ukoliko ne možete da pronađete neki proizvod,
             slobodno nas kontaktirajte.
           </p>
-          <div className={`mt-5 grid grid-cols-1 lg:grid-cols-2 gap-2`}>
-            {objects.map((object, index) => {
-              return (
-                <div
-                  key={index}
-                  className={` w-full gap-2 rounded-lg col-span-1 lg:col-span-1 p-5 hover:border-[#04b400] cursor-pointer border ${
-                    active?.id === object?.id && `border-[#04b400]`
-                  }`}
-                  onClick={() => {
-                    setActive({
-                      id: object?.id,
-                      lng: object?.lng,
-                      lat: object?.lat,
-                    });
-                    setLoading(true);
-                  }}
-                >
-                  <div className={`flex flex-col items-start gap-2 p-2`}>
-                    <Image src={Croonus} alt="" />
-                    <div className={`flex flex-col`}>
-                      <h1 className={`text-[1.2rem] font-semibold`}>
-                        {object?.name}
-                      </h1>
-                      <p className={`text-base`}>{object?.address}</p>
-                      <p className={`text-base`}>{object?.phone}</p>
+          {objects?.length <= 4 ? (
+            <div className={`mt-5 grid grid-cols-1 lg:grid-cols-2 gap-2`}>
+              {objects.map((object, index) => {
+                return (
+                  <div
+                    key={index}
+                    className={` w-full gap-2 rounded-lg col-span-1 lg:col-span-1 p-5 hover:border-[#04b400] cursor-pointer border ${
+                      active?.id === object?.id && `border-[#04b400]`
+                    }`}
+                    onClick={() => {
+                      setActive({
+                        id: object?.id,
+                        lng: object?.lng,
+                        lat: object?.lat,
+                      });
+                      setLoading(true);
+                    }}
+                  >
+                    <div className={`flex flex-col items-start gap-2 p-2`}>
+                      <Image src={Croonus} alt="" />
+                      <div className={`flex flex-col`}>
+                        <h1 className={`text-[1.2rem] font-semibold`}>
+                          {object?.name}
+                        </h1>
+                        <p className={`text-base`}>{object?.address}</p>
+                        <p className={`text-base`}>{object?.phone}</p>
+                      </div>
+                      <button
+                        className={`bg-black hover:bg-[#04b400] transition-all duration-500 w-full text-white px-5 py-2 rounded-lg`}
+                        onClick={() => {
+                          setActive((prev) => ({
+                            id: object?.id,
+                            lng: object?.lng,
+                            lat: object?.lat,
+                          }));
+                        }}
+                      >
+                        Prikaži na mapi
+                      </button>
                     </div>
-                    <button
-                      className={`bg-black hover:bg-[#04b400] transition-all duration-500 w-full text-white px-5 py-2 rounded-lg`}
-                      onClick={() => {
-                        setActive((prev) => ({
-                          id: object?.id,
-                          lng: object?.lng,
-                          lat: object?.lat,
-                        }));
-                      }}
-                    >
-                      Prikaži na mapi
-                    </button>
                   </div>
-                </div>
-              );
-            })}
-          </div>
+                );
+              })}
+            </div>
+          ) : (
+            <div className={`mt-5 lg:pb-[10rem]`}>
+              <h1 className={`text-[1.2rem] font-semibold`}>
+                Izaberite željeni grad
+              </h1>
+              <select
+                className={`w-full mt-2 p-2 rounded-lg`}
+                onChange={(e) => {
+                  const value = e.target.value;
+                  const obj = objects.find((o) => o.name === value);
+                  setActive({
+                    id: obj?.id,
+                    lng: obj?.lng,
+                    lat: obj?.lat,
+                  });
+                  setLoading(true);
+                }}
+              >
+                {objects.map((object, index) => {
+                  return (
+                    <option key={index} value={object?.name}>
+                      {object?.name}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+          )}
         </div>
         <div className={`col-span-1 md:col-span-1 2xl:col-span-2`}>
           <Map
