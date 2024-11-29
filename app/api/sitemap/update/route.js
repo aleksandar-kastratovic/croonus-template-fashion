a088931c1e98df8f5b9850646f96be5fb95cec3c
Aleksandar Kastratovic <aleksandar.kastratovic@croonus.com>
2024-11-29 16:10:43 +0100

    86c0jmeqw - Created baseURL and added as a prop at buildSitemaFile

--- app/api/sitemap/update/route.js
+++ app/api/sitemap/update/route.js
@@ -32,19 +32,26 @@ function createResponse(message, status) {
 export async function POST(req) {
   console.log("req", req);
 
-  const referer = req.headers.referer || req.headers.origin || '';
+  const referer = req.headers.referer || req.headers.origin || "";
   const clientIP = req.headers["x-forwarded-for"] || req.headers["x-real-ip"];
-  const providedSecret = req.headers["authorization"] || req.headers["x-secret-key"];
+  const providedSecret =
+    req.headers["authorization"] || req.headers["x-secret-key"];
 
-  console.log("referer",referer)
-  console.log("clientIP",clientIP)
-  console.log("providedSecret",providedSecret)
+  console.log("referer", referer);
+  console.log("clientIP", clientIP);
+  console.log("providedSecret", providedSecret);
 
   const body = await req.json();
   console.log("Received data from backend:", body);
 
+  // Dinamički izvlacenje protokola i hosta
+  const { headers } = req;
+  const protocol = headers.get("x-forwarded-proto") || "http";
+  const host = headers.get("host") || "localhost:3000";
+  const baseUrl = `${protocol}://${host}`;
+
   try {
-    await buildSitemapFile(body.files);
+    await buildSitemapFile(body.files, baseUrl);
     return createResponse("Sitemap successfully updated.", 200);
   } catch (error) {
     console.error("Error in cron job:", error.message);
